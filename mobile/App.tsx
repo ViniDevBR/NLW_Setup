9bb61c2a07feb9824d96670458ace0bf8598c726
ViniDevBR <vinigtr386@gmail.com>
2023-01-31 16:02:29 -0300

    :fire: Finish configs

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+//REACT
+import { StyleSheet, Text, View, StatusBar } from 'react-native'
+//COMPONENTS
+import { Loading } from './src/components/Loading'
+//FONTS
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold
+} from '@expo-google-fonts/inter'
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle="light-content" backgroundColor='transparent' translucent/>
+      {fontsLoaded ? (
+        <Text>Open up App.tsx to start working on your app!</Text>
+      ) : (
+        <Loading />
+      )}
     </View>
   )
 }
