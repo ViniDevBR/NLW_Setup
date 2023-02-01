f9ed32c607b1e5c271f660254dc7a1a5be43704e
ViniDevBR <vinigtr386@gmail.com>
2023-02-01 12:26:02 -0300

    :fire: Lib for start prisma client once

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,9 +1,23 @@
-import { createHabits } from './useCases/createHabits'
+//FASTIFY
 import { FastifyInstance } from 'fastify'
+//USE CASES
+import { createHabits } from './useCases/createHabits'
+import { getHabitsDay } from './useCases/getHabitsDay'
+import { toggleHabit } from './useCases/toggleHabit'
+import { summaryOfAll } from './useCases/summaryOfAll'
 
 
 export async function appRouter(app: FastifyInstance) {
 
-  //List Habits
+  //Create Habits
   app.post('/habits' , createHabits)
+
+  //List Habits of Day
+  app.get('/day', getHabitsDay)
+
+  //Toggle Habit
+  app.patch('/habits/:id/toggle', toggleHabit)
+
+  //Summary
+  app.get('/summary', summaryOfAll)
 }
