2953ed8c4d90293a13e67d9874a0f25d731312c8
ViniDevBR <vinigtr386@gmail.com>
2023-02-03 10:02:40 -0300

    :fire: Adding habistDay

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,4 +1,10 @@
+import { generateRangeForDate } from '../utils/generateRangeForDates'
+import { HabitDay } from './HabitDay'
+
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+const summaryDates = generateRangeForDate()
+const minimumSummaryDatesSize = 18 * 7 //18 weeks
+const amountDaysToFill = minimumSummaryDatesSize - summaryDates.length
 
 export function SummaryTable() {
   return (
@@ -15,8 +21,18 @@ export function SummaryTable() {
         ))}
       </section>
 
-      <section>
+      <section className="grid grid-rows-7 grid-flow-col gap-3">
+        {summaryDates.map((date) => (
+          <HabitDay key={date.toString()}/>
+        ))}
 
+        {amountDaysToFill > 0 && Array.from({ length: amountDaysToFill })
+          .map((value, index) => (
+            <div
+              key={index}
+              className="h-10 w-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+            />
+          ))}
       </section>
     </main>
   )
