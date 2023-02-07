c2aeac8a4c51429b78a63cebb443c77187fbf0f8
ViniDevBR <vinigtr386@gmail.com>
2023-02-07 17:43:39 -0300

    :fire: New component

---
+++ mobile/src/screens/Home.tsx
@@ -0,0 +1,10 @@
+import { View } from 'react-native'
+import { Header } from '../components/Header'
+
+export function Home() {
+  return (
+    <View className='bg-backgroundStock flex-1 px-8 pt-16'>
+      <Header />
+    </View>
+  )
+}
