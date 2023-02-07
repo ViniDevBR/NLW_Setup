541352e790472b0b46efe2bafa7288917e277339
ViniDevBR <vinigtr386@gmail.com>
2023-02-07 17:40:42 -0300

    :fire: Using nativeWind

---
+++ mobile/tailwind.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        backgroundStock: '#09090a'
+      },
+      fontFamily: {
+        regular: 'Inter_400Regular',
+        semibold: 'Inter_600SemiBold',
+        bold: 'Inter_700Bold',
+        extrabold: 'Inter_800ExtraBold'
+      }
+    }
+  },
+  plugins: []
+}
